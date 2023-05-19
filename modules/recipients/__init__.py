155a7c841af69ec37d4347158e0a7a2dc1418769
Lorenzo J. Lucchini <LuccoJ@users.noreply.github.com>
2023-05-19 18:09:15 +0200

    Create __init__.py

---
+++ modules/recipients/__init__.py
@@ -0,0 +1,24 @@
+"""
+alerts.py - IRC bot geographical alerts module
+
+This file is part of the "Brainstorm" distribution (https://github.com/luccoj/earthquakes-bot or http://xxx.github.io).
+Copyright (c) 2016-2023 Lorenzo J. Lucchini (ljlbox@tiscali.it)
+
+This program is free software: you can redistribute it and/or modify  
+it under the terms of the GNU General Public License as published by  
+the Free Software Foundation, version 3.
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+General Public License for more details.
+You should have received a copy of the GNU General Public License 
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from recipient import Recipient, Renderer
+from file import File
+from irc import IRC
+from reddit import Reddit
+from tweet import Twitter
+from matrix import Matrix
+from toot import Mastodon
